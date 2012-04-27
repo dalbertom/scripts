f49f5ceb02c39706b5f3bcdadf3903ed94e811a0
David Alberto <dalbertom@gmail.com>
2012-04-26 20:31:07 -0400

    main script

---
+++ work.bat
@@ -0,0 +1,33 @@
+pushd %~dp0
+if not defined JAVA_HOME set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_31
+where /q javac || path %path%;%JAVA_HOME%\bin
+
+if not defined ANT_HOME set ANT_HOME=C:\work\tools\apache-ant-1.8.2
+where /q ant || path %path%;%ANT_HOME%\bin
+
+if not defined GIT_HOME set GIT_HOME=C:\work\tools\msysgit\msysgit
+where /q git || path %path%;%GIT_HOME%\bin;%GIT_HOME%\mingw\bin;%GIT_HOME%\cmd
+
+if not defined HOME set HOME=%USERPROFILE%
+if not exist %HOME%\.ssh\id_rsa ssh-keygen -t rsa
+
+if not defined SVN_HOME set SVN_HOME=C:\Program Files (x86)\CollabNet\Subversion Client
+where /q svn || path %path%;%SVN_HOME%\bin
+
+if not defined ARAXIS_HOME set ARAXIS_HOME=c:\Program Files (x86)\Araxis\Araxis Merge v6.5 NSIS
+where /q compare || path %path%;%ARAXIS_HOME%
+
+if not defined GROOVY_HOME set GROOVY_HOME=C:\Program Files (x86)\Groovy\Groovy-1.8.4
+where /q groovy || path %path%;%GROOVY_HOME%\bin
+
+if not defined M3_HOME set M3_HOME=C:\work\tools\apache-maven-3.0.3
+where /q mvn || path %path%;%M3_HOME%\bin
+
+if exist scripts pushd scripts
+path %cd%;%path%
+popd
+popd
+prompt $+$B$D$$$T$H$H$H$G
+mode con cols=100 lines=5000
+color 17
+%*
