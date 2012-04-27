f543344b5f2a01a6600bbdb9082acbfcd5c2686c
David Alberto <dalbertom@gmail.com>
2012-04-26 20:31:06 -0400

    setup: path to useful installers

---
+++ setup.bat
@@ -0,0 +1,9 @@
+start /wait http://apache.cs.utah.edu//ant/binaries/apache-ant-1.8.2-bin.zip
+start /wait http://download.oracle.com/auth/otn/java/jdk/6u26-b03//jdk-6u26-windows-x64.exe?e=1317776938&h=bfe1cb4ccb7a6e799b5130208128103a
+REM start /wait http://msysgit.googlecode.com/files/msysGit-fullinstall-1.7.6-preview20110708.exe
+start /wait http://msysgit.googlecode.com/files/msysGit-fullinstall-1.7.7-preview20111012.exe
+start /wait http://mirrors.xmission.com/eclipse/technology/epp/downloads/release/indigo/SR1/eclipse-jee-indigo-SR1-win32-x86_64.zip
+start /wait http://www.open.collab.net/files/documents/60/4635/CollabNetSubversion-client-1.6.17-4.win32.exe
+start /wait http://usfiles.brothersoft.com/development/html_editors/epp331.exe
+start /wait http://synergy.googlecode.com/files/synergy-1.3.7-Windows-x64.exe
+start /wait http://dist.codehaus.org/groovy/distributions/installers/windows/nsis/groovy-1.8.4-installer.exe
