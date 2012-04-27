e647038b693079f4f305af24884d182f45584e1d
David Alberto <dalbertom@gmail.com>
2012-04-26 20:27:07 -0400

    svn neighborhood do ls on a couple repositories

---
+++ svn/svn-neighborhood.bat
@@ -0,0 +1,6 @@
+:: Lists some branches in subzero
+svn ls %svnrepository%/ae --verbose | sort /R
+svn ls %svnrepository%/ae/branches --verbose | sort /R
+svn ls %svnrepository%/ae/branches/personals --verbose | sort /R
+svn ls %svnrepository%/qa --verbose | sort /R
+svn ls %svnrepository%/qe --verbose | sort /R
