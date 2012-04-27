7d7a123bbc1c4443d6207feef2bf70a2c5e35cef
David Alberto <dalbertom@gmail.com>
2012-04-26 20:27:28 -0400

    proc: kill browsers, background processes and restart explorer

---
+++ proc/reclaim.bat
@@ -0,0 +1,9 @@
+taskkill /f /im chrome.exe
+taskkill /f /im iexplore.exe
+taskkill /f /im firefox.exe
+taskkill /f /im tgitcache.exe
+taskkill /f /im tsvncache.exe
+taskkill /f /im tortoisehgoverlayserver.exe
+taskkill /f /im jing.exe
+taskkill /f /im explorer.exe
+start explorer.exe
