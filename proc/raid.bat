82014693859a513630c15dac24a3e05872fb6d44
David Alberto <dalbertom@gmail.com>
2012-04-26 20:27:26 -0400

    proc: kill annoying/stuck processes

---
+++ proc/raid.bat
@@ -0,0 +1,10 @@
+taskkill /f /im tgitcache.exe
+taskkill /f /im tsvncache.exe
+taskkill /f /im tortoisehgoverlayserver.exe
+taskkill /f /im sh.exe
+taskkill /f /im git.exe
+taskkill /f /im xargs.exe
+taskkill /f /im sed.exe
+taskkill /f /im find.exe
+taskkill /f /im bash.exe
+%*
