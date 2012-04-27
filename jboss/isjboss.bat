01e1d2fc3b6429873312b4a72c9a9f1d10eaf0e9
David Alberto <dalbertom@gmail.com>
2012-04-26 20:27:40 -0400

    jboss: check if jboss is running

---
+++ jboss/isjboss.bat
@@ -0,0 +1,4 @@
+netstat -anop tcp | find ":8080" | find "LISTENING"
+netstat -anop tcp | find ":8180" | find "LISTENING"
+netstat -anop tcp | find ":8280" | find "LISTENING"
+netstat -anop tcp | find ":8380" | find "LISTENING"
