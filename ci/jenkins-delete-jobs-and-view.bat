314fe84e3b59624dc04af44a34edf269ad307965
David Alberto <dalbertom@gmail.com>
2012-08-01 15:10:14 -0400

    script to delete a view and jobs within it

---
+++ ci/jenkins-delete-jobs-and-view.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+if not defined view set view=%1
+if not defined view set /p view=view=
+if not defined JENKINS_URL set JENKINS_URL=%2
+if not defined JENKINS_CREDENTIALS set JENKINS_CREDENTIALS=%3
+if not defined JENKINS_URL set /p JENKINS_URL=JENKINS_URL=
+if not defined JENKINS_CREDENTIALS set /p JENKINS_CREDENTIALS=JENKINS_CREDENTIALS=
+
+for /f %%a in ('call jenkins-jobs %view%') do (
+  echo curl -u "%JENKINS_CREDENTIALS%" -d "" "%JENKINS_URL%/job/%%a/doDelete"
+)
+
+echo curl -u "%JENKINS_CREDENTIALS%" -d "" "%JENKINS_URL%/view/%view%/doDelete"
+endlocal
