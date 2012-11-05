e2bce577579f51de7e8a1ae88ba40cb0ad12521a
David Alberto <dalbertom@gmail.com>
2012-11-05 14:21:52 -0500

    updated test-custom

--- test/test-custom.bat
+++ test/test-custom.bat
@@ -1,2 +1,2 @@
-set CUSTOM_TEST_PATTERN=%1
-call ant -s build/build.xml tests.test -DTEST_TYPE="custom" test-reports %2 %3 %4 %5 %6 %7 %8 %9
+REM -Dstarted.engines=true -Dstarted.appserver=true -Dstarted.rdbms=true
+call ant -s build/build.xml tests.test -DTEST_TYPE="custom" -Denv.CUSTOM_TEST_PATTERN=%1 test-reports %2 %3 %4 %5 %6 %7 %8 %9
