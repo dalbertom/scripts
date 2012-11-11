9a26e28af7f793b699a119a34b8a719ea25452cf
David Alberto <dalbertom@gmail.com>
2012-11-10 22:22:15 -0500

    jenkins-test-cases with null separators instead of new lines

--- ci/jenkins-test-cases.bat
+++ ci/jenkins-test-cases.bat
@@ -8,4 +8,7 @@ if not defined JENKINS_URL set /p JENKINS_URL=JENKINS_URL=
 if not defined JENKINS_CREDENTIALS set /p JENKINS_CREDENTIALS=JENKINS_CREDENTIALS=
 
 REM curl --silent -u "%JENKINS_CREDENTIALS%" "%JENKINS_URL%/job/%job%/lastCompletedBuild/testReport/api/xml?xpath=testResult/suite/case/className&wrapper=className" | sed -e "s/<className>/\n/g" -e "s:</className>::g" | uniq
-curl --silent -u "%JENKINS_CREDENTIALS%" "%JENKINS_URL%/job/%job%/lastCompletedBuild/testReport/api/xml?xpath=testResult/suite/case/*%%5Bself::name%%20or%%20self::className%%5D&wrapper=name" | sed -e "s/<name>/\n/g" -e "s/<className>/\n/g" -e "s:</name>::g" -e "s:</className>::g" | xargs -n 2
+curl --silent -u "%JENKINS_CREDENTIALS%" "%JENKINS_URL%/job/%job%/lastSuccessfulBuild/testReport/api/xml?xpath=testResult/suite/case/*%%5Bself::name%%20or%%20self::className%%5D&wrapper=name" |sed -e "s/<name>/\x0/g" -e "s/<className>/\x0/g" -e "s:</name>::g" -e "s:</className>::g" | xargs -0 -n 2
+REM Completed
+
+
