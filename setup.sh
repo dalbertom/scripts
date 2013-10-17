7c1eef9212df41e571aabe62e7e7b88757fa3e3f
David Alberto <david.alberto@appian.com>
2013-10-16 22:15:13 -0400

    script to setup clone and execute auto scripts

---
+++ setup.sh
@@ -0,0 +1,12 @@
+if [ ! -d ~/.gist ]; then
+  git clone git@github.com:dalbertom/scripts ~/.gist
+
+  cat<<EOF>>~/.profile
+if [ -d ~/.gist ]; then
+  for i in ~/.gist/auto-*.sh; do
+    source $i
+  done
+fi
+EOF
+fi
+
