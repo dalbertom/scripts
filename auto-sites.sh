f7348c238199b87fbfc3ff499f9e61f4b0ad0f2d
David Alberto <david.alberto@appian.com>
2013-10-16 21:58:14 -0400

    ec2 sites scripts

---
+++ auto-sites.sh
@@ -0,0 +1,21 @@
+function setup-sites {
+  export EC2_HOME=$1
+  export SITE_USERNAME=$2
+  export SITE_LOG_APPSERVER=$3
+  export PATH=$EC2_HOME/bin:$PATH
+  export EC2_CERT=~/.ssh/aws.crt
+  export EC2_PRIVATE_KEY=~/.ssh/aws.key
+}
+
+function site-list {
+  ec2dtag | awk '/instance.*Name.*site:/ {print $6}'
+}
+
+alias qssh='ssh -e none -o StrictHostKeyChecking=no'
+function ssh-site {
+  qssh $SITE_USERNAME@$* || (ssh-forget $1; qssh $SITE_USERNAME@$*)
+}
+
+function site-exceptions {
+  ssh-site $* cat $SITE_LOG_APPSERVER | awk '/Exception/,/^$/ {print $0}'
+}
