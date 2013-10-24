a56791e1fdf6a81b1c41cb8bb597181cf5b6cc15
David Alberto <david.alberto@appian.com>
2013-10-24 13:10:19 -0400

    Update auto-build.sh

--- auto-build.sh
+++ auto-build.sh
@@ -3,4 +3,4 @@
 alias build="ant -s build/build.xml"
 alias qbuild="build -Dskip.gwt=true"
 alias crash="build clean-eclipse eclipse"
-alias rebuild='qbuild sound dev || build sound dev || crash sound dev || build sound clean dev'
+alias rebuild='qbuild -l build1.log sound dev || build -l build2.log sound dev || crash -l build3.log sound dev || build -l build4.log sound clean dev'
