514a46ee60f7fb3be6b60d6a6737bbb6c5182e95
David Alberto <david.alberto@appian.com>
2013-10-29 14:27:40 -0400

    Update auto-sites.sh

--- auto-sites.sh
+++ auto-sites.sh
@@ -1,12 +1,15 @@
 function setup-sites {
+  export SITE_USERNAME=$1
+  export SITE_LOG_APPSERVER=$2
+  export SITE_LOG_AUDIT=$3
+  export SITE_DEFAULT_DOMAIN=$4
+}
+
+function setup-ec2 {
   export EC2_HOME=$1
-  export SITE_USERNAME=$2
-  export SITE_LOG_APPSERVER=$3
-  export SITE_LOG_AUDIT=$4
-  export SITE_DEFAULT_DOMAIN=$5
+  export EC2_CERT=$2
+  export EC2_PRIVATE_KEY=$3
   export PATH=$EC2_HOME/bin:$PATH
-  export EC2_CERT=~/.ssh/aws.crt
-  export EC2_PRIVATE_KEY=~/.ssh/aws.key
 }
 
 function site-list {
