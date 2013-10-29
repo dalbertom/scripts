5a74a153a4d9141f6e7985aad42a29c9ee87702e
David Alberto <david.alberto@appian.com>
2013-10-29 16:56:01 -0400

    Create auto-github.sh

---
+++ auto-github.sh
@@ -0,0 +1,13 @@
+GITHUB_URL=https://github.com
+
+function github-notifications {
+  open $GITHUB_URL/notifications
+}
+
+function github-watching {
+  open $GITHUB_URL/watching
+}
+
+function github-stars {
+  open $GITHUB_URL/stars
+}
