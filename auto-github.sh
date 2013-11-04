576b0eec763ca5387e407eea06a5109e545f9b9e
David Alberto <david.alberto@appian.com>
2013-11-04 16:15:45 -0500

    Update auto-github.sh

--- auto-github.sh
+++ auto-github.sh
@@ -1,5 +1,19 @@
 GITHUB_URL=https://github.com
 
+function setup-github {
+  export GITHUB_TOKEN=$1
+  export GITHUB_AGENT=$2
+}
+
+function github-curl {
+  curl -i -H "Authorization: token $GITHUB_TOKEN" -H "User-Agent: $GITHUB_AGENT" https://api.github.com/$*
+}
+
+function github-teams-list {
+  org=$1
+  github-curl "orgs/$org/teams"
+}
+
 function github-notifications {
   open $GITHUB_URL/notifications
 }
