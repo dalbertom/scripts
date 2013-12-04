67dcd4ececc16bba8560d3e0c27db535c83765ed
David Alberto <david.alberto@appian.com>
2013-12-04 11:09:28 -0500

    Update auto-bash.sh

--- auto-bash.sh
+++ auto-bash.sh
@@ -6,3 +6,10 @@ alias top-cmd='history | sed "s/ *//" | cut -d" " -f 3 | sort | uniq -c | sort -
 alias top-git='history | grep "git " | grep -v history | sed "s/ *//" | cut -d" " -f 4 | sort | uniq -c | sort -n'
 alias today="date +%Y-%m-%d"
 alias yesterday="date -v-1d +%Y-%m-%d"
+
+function absolute {
+  rel=$1
+  pushd $rel > /dev/null
+  pwd
+  popd > /dev/null
+}
