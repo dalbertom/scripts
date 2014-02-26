c3a07347e97f7244e2fca62463d9318b0d89db48
David Alberto <david.alberto@appian.com>
2014-02-26 12:12:26 -0500

    Update auto-build.sh

--- auto-build.sh
+++ auto-build.sh
@@ -2,7 +2,7 @@
 
 alias build="ant -s build/build.xml"
 alias qbuild="build -Dskip.gwt=true"
-alias crash="build clean-eclipse eclipse || build eclipse-projects-clean eclipse-projects"
+alias crash="build clean-eclipse eclipse" #  || build eclipse-projects-clean eclipse-projects"
 alias rebuild='qbuild -l build1.log sound dev || build -l build2.log sound dev || crash -l build3.log sound dev || build -l build4.log sound clean dev'
 
 function bisectable {
