19490840538d2e63c9534c2a87e73381cfe8431a
David Alberto <david.alberto@appian.com>
2014-11-15 11:01:01 -0500

    Update auto-build.sh

--- auto-build.sh
+++ auto-build.sh
@@ -1,7 +1,7 @@
 # Aliases for internal build system
 
 alias build="ant -s build/build.xml"
-alias qbuild="build -Dskip.gwt=true"
+alias qbuild="build -Dskip.gwt=true -Dskip.checkstyle=true"
 alias crash="build clean-eclipse eclipse" #  || build eclipse-projects-clean eclipse-projects"
 alias rebuild='qbuild -l build1.log sound dev || build -l build2.log sound dev || crash -l build3.log sound dev || build -l build4.log sound clean dev'
 
