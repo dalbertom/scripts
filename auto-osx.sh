034811d03a5e885aa90c49e8d830d7059b281618
David Alberto <david.alberto@appian.com>
2015-07-28 20:20:05 -0400

    Update auto-osx.sh

--- auto-osx.sh
+++ auto-osx.sh
@@ -3,3 +3,13 @@ alias panic='killall Dock Finder SystemUIServer'
 # http://ianlunn.co.uk/articles/quickly-showhide-hidden-files-mac-os-x-mavericks/
 alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder"
 alias hideFiles="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder"
+
+function bye {
+  [ ! -z $1 ] && sleep $1
+  pmset sleepnow              
+}
+
+function brb {
+  pmset displaysleepnow
+}
+
